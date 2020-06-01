571323225c2d1d25c1188bde7b0c820cdd1f00be
Darek <darekpl9@gmail.com>
2020-06-01 14:27:01 +0200

    Initial commit.

---
+++ Algorytmy - projekty/Projekt 1 - Przeszukiwanie tablic/Projekt1/Projekt1/Program.cs
@@ -0,0 +1,256 @@
+﻿using System;
+using System.Collections;
+using System.Diagnostics;
+
+namespace Projekt1
+{
+    class Program
+    {
+        static ulong OpComparisonEQ = 0;
+        const int NIter = 10;
+
+        static ArrayList data = new ArrayList();
+
+        static bool IsPresent_LinearTim(int[] Vector, int Number)
+        {
+            for (int i = 0; i < Vector.Length; i++)
+                if (Vector[i] == Number)
+                    return true;
+            return false;
+        }
+
+        static bool IsPresent_LinearInstr(int[] Vector, int Number)
+        {
+
+            for (int i = 0; i < Vector.Length; i++)
+            {
+                OpComparisonEQ++;
+                if (Vector[i] == Number) return true;
+            }
+            return false;
+        }
+
+        static bool IsPresent_BinaryTim(int[] Vector, int Number)
+        {
+            int Left = 0, Right = Vector.Length - 1, Middle;
+            while (Left <= Right)
+            {
+                Middle = (Left + Right) / 2;
+                if (Vector[Middle] == Number) return true;
+                else if (Vector[Middle] > Number) Right = Middle - 1;
+                else Left = Middle + 1;
+            }
+            return false;
+        }
+
+        static bool IsPresent_BinaryInstr(int[] Vector, int Number)
+        {
+
+            int Left = 0, Right = Vector.Length - 1, Middle;
+            while (Left <= Right)
+            {
+                Middle = (Left + Right) / 2;
+                if (Vector[Middle] == Number)
+                {
+                    OpComparisonEQ++;
+                    return true;
+                }
+                else
+                {
+                    if (Vector[Middle] > Number) Right = Middle - 1;
+                    else Left = Middle + 1;
+
+                    OpComparisonEQ++;
+                }
+            }
+            return false;
+        }
+
+        static void LinearMaxInstr(int[] TestVector)
+        {
+            OpComparisonEQ = 0;
+
+            bool IsPresent = IsPresent_LinearInstr(TestVector, TestVector.Length - 1);
+            Console.WriteLine("Linear Max Instr " + OpComparisonEQ);
+            data.Add(OpComparisonEQ);
+
+        }
+
+        static void LinearAvgInstr(int[] TestVector)
+        {
+            OpComparisonEQ = 0;
+            bool Present;
+            for (int i = 0; i < TestVector.Length; ++i)
+                Present = IsPresent_LinearInstr(TestVector, i);
+
+            double average = (double)OpComparisonEQ / (double)TestVector.Length;
+            data.Add(average);
+
+            Console.WriteLine("Linear Avg Instr " + average.ToString("F1"));
+        }
+
+        static void LinearAvgTim(int[] TestVector)
+        {          
+                double IterationAvgTime = 0; // Czas średni jednego przejścia pętli NIter
+
+                for (int n = 0; n < (NIter + 1 + 1); ++n)
+                {
+
+                long TotalTime = 0; 
+
+                for (int i = 0; i < TestVector.Length; i++)
+                {
+                    long StartingTime = Stopwatch.GetTimestamp();
+                    bool Present = IsPresent_LinearTim(TestVector, i);
+                    long EndingTime = Stopwatch.GetTimestamp();
+
+                    TotalTime += EndingTime - StartingTime;                    
+                }
+
+                IterationAvgTime += ((double)TotalTime / TestVector.Length);
+                }
+
+                double AvgTime = (IterationAvgTime / NIter) * (1.0 / Stopwatch.Frequency) ;
+
+                data.Add(AvgTime.ToString("F8"));
+
+                Console.WriteLine("Linear Avg Tim " + AvgTime.ToString("F8"));            
+        }
+
+        static void LinearMaxTim(int[] TestVector)
+        {
+            double ElapsedSeconds;
+            long ElapsedTime = 0, MinTime = long.MaxValue, MaxTime = long.MinValue, IterationElapsedTime;
+            for (int n = 0; n < (NIter + 1 + 1); ++n)
+            {
+                long StartingTime = Stopwatch.GetTimestamp();
+                bool Present = IsPresent_LinearTim(TestVector, TestVector[TestVector.Length - 1]);
+                long EndingTime = Stopwatch.GetTimestamp();
+                IterationElapsedTime = EndingTime - StartingTime;
+                ElapsedTime += IterationElapsedTime;
+                if (IterationElapsedTime < MinTime) MinTime = IterationElapsedTime;
+                if (IterationElapsedTime > MaxTime) MaxTime = IterationElapsedTime;
+            }
+            ElapsedTime -= (MinTime + MaxTime);
+            ElapsedSeconds = ElapsedTime * (1.0 / (NIter * Stopwatch.Frequency));
+
+            data.Add(ElapsedSeconds.ToString("F8"));
+
+            Console.WriteLine("Linear Max Tim " + ElapsedSeconds.ToString("F8"));
+        }
+
+        static void BinaryMaxInstr(int[] TestVector)
+        {
+            OpComparisonEQ = 0;
+            bool Present = IsPresent_BinaryInstr(TestVector, TestVector[TestVector.Length - 1]);
+            Console.WriteLine("Binary Max Instr " + OpComparisonEQ);
+            data.Add(OpComparisonEQ);
+        }
+
+        static void BinaryMaxTim(int[] TestVector)
+        {
+            double ElapsedSeconds;
+            long ElapsedTime = 0, MinTime = long.MaxValue, MaxTime = long.MinValue, IterationElapsedTime;
+            for (int n = 0; n < (NIter + 1 + 1); ++n)
+            {
+                long StartingTime = Stopwatch.GetTimestamp();
+                bool Present = IsPresent_BinaryTim(TestVector, TestVector[TestVector.Length - 1]);
+                long EndingTime = Stopwatch.GetTimestamp();
+                IterationElapsedTime = EndingTime - StartingTime;
+                ElapsedTime += IterationElapsedTime;
+                if (IterationElapsedTime < MinTime) MinTime = IterationElapsedTime;
+                if (IterationElapsedTime > MaxTime) MaxTime = IterationElapsedTime;
+            }
+            ElapsedTime -= (MinTime + MaxTime);
+            ElapsedSeconds = ElapsedTime * (1.0 / (NIter * Stopwatch.Frequency));
+
+            data.Add(ElapsedSeconds.ToString("F10"));
+
+            Console.WriteLine("Binary Max Tim " + ElapsedSeconds.ToString("F10"));
+        }
+
+        static void BinaryAvgInstr(int[] TestVector)
+        {
+            OpComparisonEQ = 0;
+            bool Present;
+            for (int i = 0; i < TestVector.Length; ++i)
+                Present = IsPresent_BinaryInstr(TestVector, i);
+            
+            double average = (double)OpComparisonEQ / (double)TestVector.Length;
+            data.Add(average);
+
+            Console.WriteLine("Binary Avg Instr " + average.ToString("F1"));
+
+        }
+
+        static void BinaryAvgTim(int[] TestVector)
+        {
+            double IterationAvgTime = 0; // Czas średni jednego przejścia pętli NIter
+
+            for (int n = 0; n < (NIter + 1 + 1); ++n)
+            {
+                long TotalTime = 0;
+
+                for (int i = 0; i < TestVector.Length; i++)
+                {
+                    long StartingTime = Stopwatch.GetTimestamp();
+                    bool Present = IsPresent_BinaryTim(TestVector, i);
+                    long EndingTime = Stopwatch.GetTimestamp();
+
+                    TotalTime += EndingTime - StartingTime;
+                }
+
+                IterationAvgTime += ((double)TotalTime / TestVector.Length);
+            }
+
+            double AvgTime = (IterationAvgTime / NIter) * (1.0 / Stopwatch.Frequency);
+            data.Add(AvgTime.ToString("F10"));
+
+            Console.WriteLine("Binary Avg Tim " + AvgTime.ToString("F10"));
+        }
+
+        static void WriteDataToFile()
+        {
+            using (System.IO.StreamWriter file = new System.IO.StreamWriter(@"C:\Users\Darek\Desktop\Algorytmy\dane.txt"))
+            {
+                for(int i = 0; i < data.Count; i++) { 
+                    if (i % 9 == 0 && i != 0)
+                        file.WriteLine();
+
+                    file.Write(data[i]);
+
+                    if (i < data.Count - 1)
+                        file.Write(" ");
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            for (int ArraySize = 10000; ArraySize <= 1000000; ArraySize += 10000)
+            {
+                Console.WriteLine("Size " + ArraySize);
+
+                data.Add(ArraySize);
+
+                int[] TestVector = new int[ArraySize];
+
+                for (int i = 0; i < TestVector.Length; ++i)
+                    TestVector[i] = i;
+
+                
+                LinearMaxInstr(TestVector);                
+                LinearMaxTim(TestVector); 
+                BinaryMaxInstr(TestVector); 
+                BinaryMaxTim(TestVector); 
+                LinearAvgInstr(TestVector); 
+                LinearAvgTim(TestVector); 
+                BinaryAvgInstr(TestVector); 
+                BinaryAvgTim(TestVector); 
+
+                Console.Write("\n");              
+            }
+            WriteDataToFile();
+        }
+    }
+}
---
+++ Algorytmy - projekty/Projekt 2 - Liczby Pierwsze/Projekt2/Liczby Pierwsze - Projekt/Liczby Pierwsze - Projekt/Program.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Numerics;
+
+namespace Liczby_Pierwsze___Projekt
+{
+    class Program
+    {
+        static ulong DivsNum = 0;
+        static List<String> wyniki = new List<String>();
+
+        static bool AlgorytmPrzykładowy(BigInteger Num)
+        {
+            if (Num < 2) return false;
+            else if (Num < 4) return true;
+            else if (Num % 2 == 0) return false;
+            else for (BigInteger u = 3; u < Num / 2; u += 2)
+                    if (Num % u == 0) return false;
+            return true;
+        }
+        static bool AlgorytmPrzykładowyInstrumentacja(BigInteger Num)
+        {
+            DivsNum = 0;
+
+            if (Num < 2) return false;
+            else if (Num < 4) return true;
+            if (Num % 2 == 0)
+            {
+                DivsNum++;
+                return false;
+            }
+            else
+            {
+                DivsNum++;
+            }
+
+            for (BigInteger u = 3; u < Num / 2; u += 2)
+            {
+                    DivsNum++;
+                    if (Num % u == 0) return false;
+            }
+
+            return true;
+        }
+
+        static bool AlgorytmLepszy(BigInteger Num)
+        {
+            if (Num < 2) return false;
+            else if (Num < 4) return true;
+            else if (Num % 2 == 0) return false;
+            else
+            {
+                BigInteger pierwiastek;
+
+                if (Num > Int64.MaxValue){
+                    pierwiastek = PrzyblizeniePierwiastka(Num);
+                }
+                else
+                {
+                    pierwiastek = (BigInteger)Math.Sqrt((Int64)Num);
+                }
+
+
+                for (BigInteger u = 3; u <= pierwiastek; u += 2)
+                    if (Num % u == 0) return false;
+            }
+            return true;
+        }
+
+        static bool AlgorytmLepszyInstrumentacja(BigInteger Num)
+        {
+            DivsNum = 0;
+
+            if (Num < 2) return false;
+            else if (Num < 4) return true;
+            if (Num % 2 == 0)
+            {
+                DivsNum++;
+                return false;
+            }
+            else
+            {
+                DivsNum++;
+            }
+
+                BigInteger pierwiastek;
+
+                if (Num > Int64.MaxValue)
+                {
+                    pierwiastek = PrzyblizeniePierwiastka(Num);
+                }
+                else
+                {
+                    pierwiastek = (BigInteger)Math.Sqrt((Int64)Num);
+                }
+
+                for (BigInteger u = 3; u <= pierwiastek; u += 2)
+                {
+                    DivsNum++;
+                    if (Num % u == 0) return false;
+                }
+
+            return true;
+        }
+
+        static bool AlgorytmJeszczeLepszy(BigInteger Num)
+        {
+            if (Num < 2) return false;
+            else if (Num < 4) return true;
+            else if (Num % 2 == 0 || Num % 3 == 0) return false;
+            else
+            {
+                BigInteger pierwiastek;
+
+                if (Num > Int64.MaxValue)
+                {
+                    pierwiastek = PrzyblizeniePierwiastka(Num);
+                }
+                else
+                {
+                    pierwiastek = (BigInteger)Math.Sqrt((Int64)Num);
+                }
+
+
+                for (BigInteger u = 6; u <= pierwiastek; u += 6)
+                {
+                    if (Num % (u - 1) == 0) return false;
+                    if (Num % (u + 1) == 0) return false;
+                }
+            }
+
+            return true;
+        }
+
+        static bool AlgorytmJeszczeLepszyInstrumentacja(BigInteger Num)
+        {
+            DivsNum = 0;
+
+            if (Num < 2) return false;
+            else if (Num < 4) return true;
+
+            if (Num % 2 == 0 || Num % 3 == 0)
+            {
+                DivsNum += 2;
+                return false;
+            }
+            else
+            {
+                DivsNum += 2;
+            }
+
+            BigInteger pierwiastek;
+
+            if (Num > Int64.MaxValue)
+            {
+                pierwiastek = PrzyblizeniePierwiastka(Num);
+            }
+            else
+            {
+                pierwiastek = (BigInteger)Math.Sqrt((Int64)Num);
+            }
+
+
+            for (BigInteger u = 6; u <= pierwiastek; u += 6)
+            {
+                DivsNum++;
+                if (Num % (u - 1) == 0) return false;
+                DivsNum++;
+                if (Num % (u + 1) == 0) return false;
+            }
+            
+            return true;
+        }
+
+        static BigInteger PrzyblizeniePierwiastka(BigInteger Num, int ilosc_przyblizen = 50)
+        {
+            BigInteger x = Num;     // Działa dobrze tylko dla bardzo dużych liczb, ponieważ
+                                   //  przy dzieleniu nie uwzględniana jest część dziesiętna, ze względu na
+                                  //   typ zmiennych
+
+            for(int i = 0; i < ilosc_przyblizen; i++)
+            {
+                BigInteger f0 = BigInteger.Multiply(x, x) - Num; // Wartość funkcji - (x^2 - Num)
+                BigInteger f1 = BigInteger.Multiply(x, 2);      //  Wartość pierwszej pochodnej funkcji f0 - (2 * x)
+
+                BigInteger div = BigInteger.Divide(f0, f1);
+
+                x -= div;
+            }
+
+            return x;
+        }
+
+        static void ZapiszWynikiDoPliku()
+        {
+            Console.WriteLine("Zapisywanie wyników do pliku.");
+
+            using (System.IO.StreamWriter file = new System.IO.StreamWriter(@"C:\Users\Darek\Desktop\Studia\Algorytmy\Projekt2\dane.txt"))
+            {
+                for (int i = 0; i < wyniki.Count; i++)
+                {
+                    if (i % 4 == 0 && i != 0)
+                        file.WriteLine();
+
+                    file.Write(wyniki[i]);
+
+                    if (i < wyniki.Count - 1)
+                        file.Write(" ");
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            String[] PrimeNums = new String[]{ "101", "1009", "10091", "100913", "1009139",
+                                               "1009140611", "10091406133", "100914061337", "1009140613399"};
+
+            Console.WriteLine("Test pierwszości liczb.");
+
+            foreach (String number in PrimeNums)
+            {
+                BigInteger Num = BigInteger.Parse(number);
+                wyniki.Add(number);
+
+                if (Num <= 1009140611) // Zmniejszam zakres dla algorytmu podstawowego, 
+                                      //  ze względu na bardzo długi czas jego wykonywania dla większych liczb
+                {
+                    AlgorytmPrzykładowyInstrumentacja(Num); 
+                    wyniki.Add(DivsNum.ToString());
+                }
+                else
+                {
+                    wyniki.Add("0"); // Dodaję 0, aby w pliku z wynikiami zachować poprawność wyświetlania
+                }
+
+                AlgorytmLepszyInstrumentacja(Num);
+                wyniki.Add(DivsNum.ToString());
+
+                AlgorytmJeszczeLepszyInstrumentacja(Num);
+                wyniki.Add(DivsNum.ToString());
+
+            }
+
+
+           // ZapiszWynikiDoPliku();
+
+
+        }
+    }
+}
